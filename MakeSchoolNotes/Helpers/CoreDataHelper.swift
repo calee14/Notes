9e7bc156dfe19b40478953e6853565d6b60e3702
calee14 <caplee1000@gmail.com>
2018-07-05 10:41:20 -0700

    created coredata helper

--- MakeSchoolNotes/Helpers/CoreDataHelper.swift
+++ MakeSchoolNotes/Helpers/CoreDataHelper.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreData
 
 struct CoreDataHelper {
+    
     static let context: NSManagedObjectContext = {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
             fatalError()
@@ -20,4 +21,39 @@ struct CoreDataHelper {
         
         return context
     }()
+    
+    static func newNote() -> Note {
+        let note = NSEntityDescription.insertNewObject(forEntityName: "Note", into: context) as! Note
+        
+        return note
+    }
+    
+    static func saveNote() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Could not save \(error.localizedDescription)")
+        }
+    }
+    
+    static func deleteNote(_ note: Note) {
+        context.delete(note)
+        
+        saveNote()
+    }
+    
+    static func retrieveNotes() -> [Note] {
+        // create a NSFetchRequest to retrieve all Person objects
+        let fetchRequest = NSFetchRequest<Note>(entityName: "Note")
+        
+        // use the NSManagedObjectContext to execute the NSFetchRequest
+        do {
+            let results = try context.fetch(fetchRequest)
+            return results
+        } catch let error {
+            print("Could not fetch \(error.localizedDescription)")
+            return [Note]()
+        }
+        
+    }
 }
